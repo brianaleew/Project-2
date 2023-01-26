e1b2158c1f90e0cf0f35a9b392ffe25866d52b74
Briana Wright <brianaleew@gmail.com>
2023-01-26 09:50:45 -0500

    Edited order of collectionControllers to make feed available to all users, fixed bug in myColletion page, removed unnecessary nav links from navbar

--- controllers/collectionControllers.js
+++ controllers/collectionControllers.js
@@ -17,37 +17,8 @@ const router = express.Router()
 //// Routes               ////
 //////////////////////////////
 
-// Router Middleware
-// Authorization middleware
-// If you have some resources that should be accessible to everyone regardless of loggedIn status, this middleware can be moved, commented out, or deleted. 
-router.use((req, res, next) => {
-	// checking the loggedIn boolean of our session
-	if (req.session.loggedIn) {
-		// if they're logged in, go to the next thing(thats the controller)
-		next()
-	} else {
-		// if they're not logged in, send them to the login page
-		res.redirect('/auth/login')
-	}
-})
-
-// Routes
-
-// index ALL -> A collection of ALL products and wellnessTips from ALL USERS (including seeds)
-
-// router.get('/feed', (req, res) => {
-// 	Product.find({})
-// 		.then(products => {
-// 			const username = req.session.username
-// 			const loggedIn = req.session.loggedIn
-			
-// 			res.render('products/index', { products, username, loggedIn })
-// 		})
-// 		.catch(error => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
+//index ALL -> A collection of ALL products and wellnessTips from ALL USERS (including seeds)
+// placed above middleware to make it accessible to all users,not just those with an account
 router.get('/feed', (req, res) => {
 	// let allProducts
 	const { username, loggedIn } = req.session 
@@ -69,6 +40,20 @@ router.get('/feed', (req, res) => {
 	})
 })
 
+// Router Middleware
+// Authorization middleware 
+router.use((req, res, next) => {
+	// checking the loggedIn boolean of our session
+	if (req.session.loggedIn) {
+		// if they're logged in, go to the next thing(thats the controller)
+		next()
+	} else {
+		// if they're not logged in, send them to the login page
+		res.redirect('/auth/login')
+	}
+})
+
+// Routes
 
 //index ALL -> A collection of ALL products and wellnessTips for 
 // LOGGED IN USER ONLY!
@@ -77,11 +62,11 @@ router.get('/mine', (req, res) => {
 	const { username, userId, loggedIn } = req.session 
 	Product.find({ owner: userId })
 		.then( products => {
-			userProducts = products
-			console.log('This is USER PRODUCTS:', userProducts)
+			
+			
 			WellnessTip.find({ owner: userId })
 				.then( wellnessTips => {
-					res.render('wellnessTips/index', { userProducts, wellnessTips, username, loggedIn })
+					res.render('collection/mine', { products, wellnessTips, username, loggedIn })
 
 				})
 				.catch(error => res.redirect(`/error?error=${error}`))

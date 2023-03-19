384e0a244f5c18c5b7a8ca1d50f3961883e7c350
Briana Wright <brianaleew@gmail.com>
2023-03-19 15:06:11 -0400

    ready for deploy

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -32,26 +32,26 @@ router.use((req, res, next) => {
 
 //TEMPORARY SEED ROUTE
 
-router.get('/seed', (req, res) => {
-    // array of starter resources(fruits)
-    const starterProducts = [
-        {name: 'Hyaloronic Acid Airy Sun Stick', brand: 'Isntree' , goodFor: ['UV Sun protection'], skinType: 'All', description: 'One of my favorite korean sunscreens. The stick makes it easy to reapply on the go!' , personalRating: 9.5, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDou99rW67JVTq2kCAsHj7RqEjR-xnfJNDVA&usqp=CAU', productLink: 'https://oo35mm.com/products/isntree-hyaluronic-acid-airy-sun-stick?_pos=1&_sid=8b052bf54&_ss=r'  },
-        {name: 'Glow Serum', brand: 'Beauty of Joseon' , goodFor: ['skin-brightening', ' anti-inflammation'], skinType: 'All', description: 'Makes my skin feel amazing and improved my overall complexion. Propolis extract makes for a great natural option' , personalRating: 8, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAnza2GJO_hj4c7CyYCtygmTeSw7Ig6HKm6Q&usqp=CAU', productLink: 'https://oo35mm.com/products/beauty-of-joseon-glow-serum?_pos=1&_sid=d63ddd8bd&_ss=r' },
-        {name: 'Cicaful Ampoule', brand: 'Beplain' , goodFor: ['strengthening the skin barrier', 'calming the skin'], skinType: 'All', description: 'One of my favorite korean sunscreens. The stick makes it easy to reapply on the go!' , personalRating: 7, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrJXypz3paWxcQaTHkoY375ACKVEFJsmOlheRspvMRhP78Ne4jzYkXmPAAq0c4tWu8o0&usqp=CAU', productLink: 'https://oo35mm.com/products/beplain-cicaful-ampoule-i?_pos=1&_sid=7fe1c8722&_ss=r' },
-        {name: 'Tea Tree Healing Essential Mask', brand: 'Mediheal' , goodFor: ['oil control', 'calming the skin'], skinType: 'oily, combination', description: 'This mask is the perfect pick-me-up for my skin when its feeling unruly.' , personalRating: 8.5, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVPUI1PGN5ILgFFW9hnWaCCAeP8PfQvyFaug&usqp=CAU', productLink: 'https://oo35mm.com/products/mediheal-tea-tree-healing-solution-essential-mask?_pos=1&_sid=9635f7b53&_ss=r' },
-    ]
-    // then we delete every fruit in the database(all instances of this resource)
-    Product.deleteMany({})
-        .then(() => {
-            // then we'll seed(create) our starter fruits
-            Product.create(starterProducts)
-                // tell our db what to do with success and failures
-                .then(data => {
-                    res.json(data)
-                })
-                .catch(err => console.log('The following error occurred: \n', err))
-        })
-})
+// router.get('/seed', (req, res) => {
+//     // array of starter resources(fruits)
+//     const starterProducts = [
+//         {name: 'Hyaloronic Acid Airy Sun Stick', brand: 'Isntree' , goodFor: ['UV Sun protection'], skinType: 'All', description: 'One of my favorite korean sunscreens. The stick makes it easy to reapply on the go!' , personalRating: 9.5, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDou99rW67JVTq2kCAsHj7RqEjR-xnfJNDVA&usqp=CAU', productLink: 'https://oo35mm.com/products/isntree-hyaluronic-acid-airy-sun-stick?_pos=1&_sid=8b052bf54&_ss=r'  },
+//         {name: 'Glow Serum', brand: 'Beauty of Joseon' , goodFor: ['skin-brightening', ' anti-inflammation'], skinType: 'All', description: 'Makes my skin feel amazing and improved my overall complexion. Propolis extract makes for a great natural option' , personalRating: 8, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAnza2GJO_hj4c7CyYCtygmTeSw7Ig6HKm6Q&usqp=CAU', productLink: 'https://oo35mm.com/products/beauty-of-joseon-glow-serum?_pos=1&_sid=d63ddd8bd&_ss=r' },
+//         {name: 'Cicaful Ampoule', brand: 'Beplain' , goodFor: ['strengthening the skin barrier', 'calming the skin'], skinType: 'All', description: 'One of my favorite korean sunscreens. The stick makes it easy to reapply on the go!' , personalRating: 7, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrJXypz3paWxcQaTHkoY375ACKVEFJsmOlheRspvMRhP78Ne4jzYkXmPAAq0c4tWu8o0&usqp=CAU', productLink: 'https://oo35mm.com/products/beplain-cicaful-ampoule-i?_pos=1&_sid=7fe1c8722&_ss=r' },
+//         {name: 'Tea Tree Healing Essential Mask', brand: 'Mediheal' , goodFor: ['oil control', 'calming the skin'], skinType: 'oily, combination', description: 'This mask is the perfect pick-me-up for my skin when its feeling unruly.' , personalRating: 8.5, productImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVPUI1PGN5ILgFFW9hnWaCCAeP8PfQvyFaug&usqp=CAU', productLink: 'https://oo35mm.com/products/mediheal-tea-tree-healing-solution-essential-mask?_pos=1&_sid=9635f7b53&_ss=r' },
+//     ]
+//     // then we delete every fruit in the database(all instances of this resource)
+//     Product.deleteMany({})
+//         .then(() => {
+//             // then we'll seed(create) our starter fruits
+//             Product.create(starterProducts)
+//                 // tell our db what to do with success and failures
+//                 .then(data => {
+//                     res.json(data)
+//                 })
+//                 .catch(err => console.log('The following error occurred: \n', err))
+//         })
+// })
 
 
 
--- controllers/wellnessTipControllers.js
+++ controllers/wellnessTipControllers.js
@@ -20,26 +20,26 @@ router.use((req, res, next) => {
 })
 
 //TEMP SEED DATA FOR TESTING 
-router.get('/seed', (req, res) => {
-    // array of starter tips
-    const starterWellnessTips = [
-        {title: 'Raspberry Tea', description: 'this tea has helped me significantly decrease my period cramps', goodFor: ['menstrual cramps'], source: 'google.com', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdf1pBO_CJxKetBMhmPRMjCSWFrQGNQqNQhw&usqp=CAU' },
-        {title: 'Malasana (Garland Pose)', description: 'This pose stretches the lower body making it great for lower back pain!', source: 'https://www.yogajournal.com/poses/yoga-by-benefit/back-pain/yoga-lower-back-pain/', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTlAWYbVOR8zZb11KqtxmdhVcUxMk9QeXacQ&usqp=CAU'},
-		{title: 'Vitamin B12', description: 'after taking these for the past month, I can truly say that I have increased energy and go longer without needing my coffee and blah blah blah.', goodFor: ['energy'], source: 'google.com', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT30pdF1X847MP6eyvDZ2wH5UhNIc0AFaConCFFh9BvhvHY4_O9kOz1SjaPrgCwtAZ3aeo&usqp=CAU' },
-		{title: 'Magnesium Glycinate', description: 'being deficient in magnesium sucks but with these supplements i have had better sleep and have healthier poops and stuff dude', goodFor: ['better sleep', ' constipation', ' overall mood boost'], source: 'google.com', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLRPyIt-W8zUk5pjjz-GGwTgyti2BuJWshig&usqp=CAU' },
-		{title: 'Humidifiers are great!', description: 'The air in Arizona is soo dry so investing in a humidifier is great blah blah blah benefits benefits', goodFor: ['good stuff', ' other good stuff', 'you get it'], tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrZa_yzATtPSo6gyz8Nz-npcDlU7RPR6BKdA&usqp=CAU' },
-    ]
-    WellnessTip.deleteMany({})
-        .then(() => {
-            // then we'll seed(create) our starter fruits
-            WellnessTip.create(starterWellnessTips)
-                // tell our db what to do with success and failures
-                .then(data => {
-                    res.json(data)
-                })
-                .catch(err => console.log('The following error occurred: \n', err))
-        })
-})
+// router.get('/seed', (req, res) => {
+//     // array of starter tips
+//     const starterWellnessTips = [
+//         {title: 'Raspberry Tea', description: 'this tea has helped me significantly decrease my period cramps', goodFor: ['menstrual cramps'], source: 'google.com', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdf1pBO_CJxKetBMhmPRMjCSWFrQGNQqNQhw&usqp=CAU' },
+//         {title: 'Malasana (Garland Pose)', description: 'This pose stretches the lower body making it great for lower back pain!', source: 'https://www.yogajournal.com/poses/yoga-by-benefit/back-pain/yoga-lower-back-pain/', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTlAWYbVOR8zZb11KqtxmdhVcUxMk9QeXacQ&usqp=CAU'},
+// 		{title: 'Vitamin B12', description: 'after taking these for the past month, I can truly say that I have increased energy and go longer without needing my coffee and blah blah blah.', goodFor: ['energy'], source: 'google.com', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT30pdF1X847MP6eyvDZ2wH5UhNIc0AFaConCFFh9BvhvHY4_O9kOz1SjaPrgCwtAZ3aeo&usqp=CAU' },
+// 		{title: 'Magnesium Glycinate', description: 'being deficient in magnesium sucks but with these supplements i have had better sleep and have healthier poops and stuff dude', goodFor: ['better sleep', ' constipation', ' overall mood boost'], source: 'google.com', tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLRPyIt-W8zUk5pjjz-GGwTgyti2BuJWshig&usqp=CAU' },
+// 		{title: 'Humidifiers are great!', description: 'The air in Arizona is soo dry so investing in a humidifier is great blah blah blah benefits benefits', goodFor: ['good stuff', ' other good stuff', 'you get it'], tipImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrZa_yzATtPSo6gyz8Nz-npcDlU7RPR6BKdA&usqp=CAU' },
+//     ]
+//     WellnessTip.deleteMany({})
+//         .then(() => {
+//             // then we'll seed(create) our starter fruits
+//             WellnessTip.create(starterWellnessTips)
+//                 // tell our db what to do with success and failures
+//                 .then(data => {
+//                     res.json(data)
+//                 })
+//                 .catch(err => console.log('The following error occurred: \n', err))
+//         })
+// })
 
 // Routes
 
